b045d5c9f50116f7d871eb479ad9202fb0332be3
Harabiih <hauwahrabii@gmail.com>
2025-02-19 16:35:52 +0100

    Access token gotton

--- config/initializers/doorkeeper.rb
+++ config/initializers/doorkeeper.rb
@@ -1,15 +1,21 @@
-
 Doorkeeper.configure do
   orm :active_record
 
-  resource_owner_from_credentials do |routes|
+  resource_owner_authenticator do
+    User.find_by(id: session[:user_id]) || redirect_to(new_user_session_url)
+  end
+
+  resource_owner_from_credentials do |_routes|
     user = User.find_for_authentication(email: params[:username])
     if user&.valid_password?(params[:password])
       user
     end
   end
 
-  # ✅ Define default and optional scopes
-  default_scopes :public
-  optional_scopes :read, :write
+  grant_flows %w[password client_credentials authorization_code]
+
+  access_token_expires_in 2.hours
+  use_refresh_token
 end
+
+
