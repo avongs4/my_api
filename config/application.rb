1db9207dc1cb9547605c7d9d565e44d574912352
Harabiih <hauwahrabii@gmail.com>
2025-03-10 11:36:09 +0100

    Solidcable and cache has been remove and cleared

--- config/application.rb
+++ config/application.rb
@@ -1,34 +1,3 @@
-# require_relative "boot"
-
-# require "rails/all"
-
-# # Require the gems listed in Gemfile, including any gems
-# # you've limited to :test, :development, or :production.
-# Bundler.require(*Rails.groups)
-
-# module MyApi
-#   class Application < Rails::Application
-#     # Initialize configuration defaults for originally generated Rails version.
-#     config.load_defaults 8.0
-
-#     config.autoload_lib(ignore: %w[assets tasks])
-
-#     config.middleware.use ActionDispatch::Flash
-
-#     config.api_only = true
-
-#     # ✅ Move Rack::Cors inside the class block
-#     config.middleware.insert_before 0, Rack::Cors do
-#       allow do
-#         origins '*'
-#         resource '*', headers: :any, methods: [:get, :post, :put, :delete, :options]
-#       end
-#     end
-#   end
-# end
-
-
-
 
 require_relative "boot"
 
